c583c0fa06a6a48bbd5b98326b39f28ee1c3428b
Neo Sejwane <sejwanebls@gmail.com>
2026-05-23 11:48:46 +0200

    Initial commit: Clean boilerplate for The Battlefield

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return(
+      <div>
+        <h1>Welcome To The battlefield</h1>
+          <p>Where Magic Happens .</p>
+        
+      </div>
+
+  )
+
+}
+
+
+export default App
